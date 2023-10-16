3f0fa36fe0df3228f29fe0acc21a2345acad0234
mghlaiel <mghlaiel@gmail.com>
2023-10-16 12:05:40 +0100

    re-commit initial template
    Signed-off-by: mghlaiel <mghlaiel@gmail.com>

--- sky-agency-v1.3.0/agency-skygency/documentation/html-docs/README.md
+++
@@ -1,2 +0,0 @@
-# nirwana-html-docs
-Nirwana HTML Documentation
--- sky-agency-v1.3.0/agency-skygency/html/src/js/footer-contact.js
+++
@@ -1,9 +0,0 @@
-// Validate form
-var toastHTML = '<span>Message Sent</span><button onclick="M.Toast.dismissAll()" class="btn-icon waves-effect toast-action"><i class="material-icons">close</i></button>';
-$.validate({
-  form: "#contact_form",
-  onSuccess: function(form) {
-    M.toast({html: toastHTML});
-    return false;
-  }
-});
